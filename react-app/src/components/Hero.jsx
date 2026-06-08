6be8373a4069c77e56ee8d87d4734f9fd04b2ef4
armaanmultani2008 <armaanmultani2008@gmail.com>
2026-06-08 14:41:17 +0200

    aggiunta hero

---
+++ react-app/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import './Hero.css'
+
+export default function Hero(){
+    return (
+        <section className={'hero'}>
+            <div className={'hero-content'}>
+                <span className={'hero-badge'}>Wildlife Conservation</span>
+                <h1>Saving Cheetahs, One Spot at a Time</h1>
+                <p>
+                    Dedicating to protecting cheetahs as a species through specialized care, captive breeding, and 
+                </p>
+            </div>
+        </section>
+    )
+}
